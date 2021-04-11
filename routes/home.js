8d280d82e48ce2f593f176774455d1619882b806
gedunlap <g.e.dunlap90@gmail.com>
2021-04-11 11:32:08 -0400

    auth placeholder routes function

--- routes/home.js
+++ routes/home.js
@@ -10,10 +10,34 @@ const router = require("express").Router()
 ///////////////////////////////
 // Router Routes
 ////////////////////////////////
+
 router.get("/", (req, res) => {
     res.render("home")
 })
 
+// Signup
+router.get('/auth/signup', (req, res) => {
+    res.send('signup get')
+  })
+  
+  router.post('/auth/signup', (req, res) => {
+    res.send('signup post')
+  })
+
+  // Login
+  router.get('/auth/login', (req, res) => {
+    res.send('login get')
+  })
+  
+  router.post('/auth/login', (req, res) => {
+    res.send('login post')
+  })
+  
+ // Logout 
+  router.get('/auth/logout', (req, res) => {
+    res.send('logout get')
+  })
+  
 ///////////////////////////////
 // Export Router
 ////////////////////////////////
