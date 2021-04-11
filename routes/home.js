a687c7caf24310c2fbcfec14816a45ccfd584e0a
gedunlap <g.e.dunlap90@gmail.com>
2021-04-11 12:05:26 -0400

    hashes password and creates user account

--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,8 @@
 // Import Router
 ////////////////////////////////
 const router = require("express").Router()
+const bcrypt = require('bcrypt')
+const Client = require('../models/User')
 
 ///////////////////////////////
 // Router Specific Middleware
@@ -20,23 +22,30 @@ router.get('/auth/signup', (req, res) => {
     res.render('auth/signup')
   })
   
-  router.post('/auth/signup', (req, res) => {
-    res.send('signup post')
-  })
+router.post('/auth/signup', async (req, res) => {
+    try {
+        const salt = await bcrypt.genSalt(10); //make the salt
+        req.body.password = await bcrypt.hash(req.body.password, salt); //use the salt on the user password
+        await Client.create(req.body) //make the user account
+        res.redirect('/auth/login') //go to login page
+    } catch (error) {
+        res.json(error)
+    }
+})
 
-  // Login
-  router.get('/auth/login', (req, res) => {
+// Login
+router.get('/auth/login', (req, res) => {
     res.send('login get')
-  })
-  
-  router.post('/auth/login', (req, res) => {
-    res.send('login post')
-  })
+})
 
- // Logout 
-  router.get('/auth/logout', (req, res) => {
+router.post('/auth/login', (req, res) => {
+    res.send('login post')
+})
+
+// Logout 
+router.get('/auth/logout', (req, res) => {
     res.send('logout get')
-  })
+})
   
 ///////////////////////////////
 // Export Router
