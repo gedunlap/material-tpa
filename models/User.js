6576abd0fd1ba0044093bcd65aa29a73d86446f8
gedunlap <g.e.dunlap90@gmail.com>
2021-04-11 11:21:00 -0400

    Appointment and Client Schemas export

--- models/User.js
+++ models/User.js
@@ -2,8 +2,25 @@
 const {Schema, model} = require('../db/connection.js')
 
 // Appointment Schema
-const Appointment = new Schema ({
-    name: {type: String, required: true},
-    date: {type: String, require: true},
-    process: {enum: ['Consultation', 'Cut', 'Color', 'Cut & Color'], required: true}
+const Apmt = new Schema ({
+    name: String,
+    date: String,
+    process: String
 })
+
+// Client Schema
+const ClientSchema = new Schema ({
+    username: {type: String, unique: true, required: true},
+    password: {type: String, required: true},
+    first: {type: String, required: true},
+    last: {type: String, required: true},
+    color: String,
+    type: String,
+    apmts: [Apmt]
+}, {timestamps: true})
+
+// Client Model
+const Client = model("Client", ClientSchema)
+
+// Export
+module.exports = Client
